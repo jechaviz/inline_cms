9886bc14ef69123852a881283fe96ea8e3c120a1
jechaviz <jesus.cgalaviz@gmail.com>
2025-03-03 19:21:12 -0600

    fixed local managment

--- bootstrap.php
+++ bootstrap.php
@@ -12,7 +12,14 @@
     session_start();
 
     define('ROOT_PATH', dirname(__FILE__));
-    define('ROOT_URL', str_replace(rtrim($_SERVER['DOCUMENT_ROOT'], '/'), '', str_replace(DIRECTORY_SEPARATOR, '/', ROOT_PATH)));
+    
+    // Determine if using built-in PHP server
+    if (php_sapi_name() == 'cli-server') {
+        define('ROOT_URL', '');
+    } else {
+        // For Apache/Nginx setup
+        define('ROOT_URL', str_replace(rtrim($_SERVER['DOCUMENT_ROOT'], '/'), '', str_replace(DIRECTORY_SEPARATOR, '/', ROOT_PATH)));
+    }
 
     mb_internal_encoding('UTF-8');
 
